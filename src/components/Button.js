e69c5f53ae62e34f4deb9cd7dbc9030bd25f0b1c
InNomeneDeorum <OliverCropp1966@gmail.com>
2021-12-08 17:04:36 +0300

    installed some necessary modules and implemented basic structure

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+const Button = ({btnClass, text, onClick}) => {
+    return (
+        <button className={btnClass+' '+' btn'} onClick={onClick}>{text}</button>
+    )
+}
+
+Button.defaultProps = {
+    btnClass: 'btn-regular',
+    text: 'click',
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    btnClass: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Create a new task</h1>
+            {/* <Link to='/tasks-manipulations'>Create New Tasks</Link> */}
+            
+        </header>
+    )
+}
+
+export default Header
